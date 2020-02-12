beb5fbf49319575e40eea2a3981666c34b5b92e1
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-12 16:52:32 +0000

    initiated client setup

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.0.236',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
