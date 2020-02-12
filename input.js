058f59bdf8a73f3f204ed129eaa831b9582cdf08
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-12 20:07:39 +0000

    completed program

---
+++ input.js
@@ -0,0 +1,29 @@
+let connection
+
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    console.log("Thanks for using me, ciao!")
+    process.exit();
+  } else if (data === 'w'){
+    connection.write('Move: up')
+  } else if (data === 'a'){
+    connection.write('Move: left')
+  } else if (data === 's'){
+    connection.write('Move: down')
+  } else if (data === 'd'){
+    connection.write('Move: right')
+  }
+};
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+
+  return stdin;
+}
+
+module.exports = {setupInput}
