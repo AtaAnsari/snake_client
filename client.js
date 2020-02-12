37b61a04208a1c155f397cff698e7008cd4d940f
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-12 18:10:10 +0000

    wrote name to server upon connection

--- client.js
+++ client.js
@@ -10,6 +10,10 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on('connect' ,() => {
+    console.log("Successfully connected to game server")
+    conn.write('Name: Ata')
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
