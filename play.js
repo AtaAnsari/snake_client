6aeb93f8db37d69b7cafc518a15024745935fd9c
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-12 19:00:06 +0000

    set up the user interface, taking used commands via stdin

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const {connect} = require('./client')
 console.log('Connecting ...');
 
+const setupInput = function() {
+  const handleUserInput = function(data) {
+    if (data === '\u0003') {
+      console.log("Thanks for using me, ciao!")
+      process.exit();
+    }
+  }
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+
+  return stdin;
+}
+
 connect();
+
+setupInput();
